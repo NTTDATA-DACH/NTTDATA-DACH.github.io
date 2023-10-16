f35f0df9870541c30a1e541f58e7a9553ef75748
Johannes Reim <johannes.reim@nttdata.com>
2023-10-16 20:30:32 +0200

    Layout updated for OneNTT

--- js/randomize_title.js
+++ js/randomize_title.js
@@ -12,3 +12,23 @@ if (document.readyState === "complete" || document.readyState === "interactive")
 } else {
   document.addEventListener("DOMContentLoaded", randomizeTitle);
 }
+
+function switchMode() {
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  var targetTheme = "light";
+
+  if (currentTheme === "light") {
+      targetTheme = "dark";
+  }
+
+  document.documentElement.setAttribute('data-theme', targetTheme)
+  localStorage.setItem('theme', targetTheme);
+} 
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+
+
