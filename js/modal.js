9ef96b22176cf98250744308471b0ea3ad057eb6
Johannes Reim <johannes.reim@nttdata.com>
2024-01-18 12:25:27 +0100

    Fixed z-index for image modal

--- js/modal.js
+++ js/modal.js
@@ -8,17 +8,27 @@ function openModal(imageId) {
     const picture = document.getElementById(imageId);
     const img = picture.getElementsByTagName("img")[0];
     const source = picture.getElementsByTagName("source")[0];
+    const caption = img.getAttribute("caption");
+    const ref = img.getAttribute("ref");
+
+    //modal
     const modalImg = document.getElementById("modalImg");
     const captionText = document.getElementById("caption");
     
     // Display modal
     modal.style.display = "block";
-    if (theme === "light") {
-        modalImg.src = img.src;
-    } else {
+    modalImg.src = img.src;
+    if (theme === "dark" && typeof source != 'undefined'){
         modalImg.src = source.srcset;
     }
-    captionText.innerHTML = img.alt;
+    if (typeof caption != 'undefined'){
+        captionText.innerHTML = caption;
+
+        if (typeof ref != 'undefined' && ref != null){
+            captionText.setAttribute("ref", "Figure " + ref + ". ");
+        }
+    }
+    
 
     // The element that closes the modal
     const close = document.getElementsByClassName("modal-close")[0];
